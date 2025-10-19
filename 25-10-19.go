d78016ce15c33d867b605d4189f863397888980a
The Hammer of God <2475253520@qq.com>
2025-10-19 22:11:17 +0800

    practice1

---
+++ 25-10-19.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a = 23
+	fmt.Println(a)
+	b := 24
+	fmt.Println(b)
+	fmt.Print("用Print：")
+	fmt.Print("Hello")
+	fmt.Print("world\n")
+	fmt.Println("I", "love", "you")
+	fmt.Println("hello", "world")
+	fmt.Println("tell", "hello")
+	fmt.Printf("%d\n", a)
+	fmt.Printf("%d\n", b)
+	fmt.Printf("%d\n", a)
+	var name string
+	var age int
+	fmt.Println("Input:")
+	_, err := fmt.Scanln(&name, &age)
+	if err != nil {
+		return
+	}
+	fmt.Println(name, age)
+
+}
